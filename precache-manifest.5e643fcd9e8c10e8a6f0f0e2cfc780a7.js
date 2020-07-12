3082b71551a433c446b9bd38f90fe5b3c62a8851
harry-chiu <harrychiu1106@gmail.com>
2020-07-12 12:45:50 +0800

    Updates

---
+++ precache-manifest.5e643fcd9e8c10e8a6f0f0e2cfc780a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09c41b6d39f5d11977fc75da000032c",
+    "url": "/coupon-advertising/index.html"
+  },
+  {
+    "revision": "0af337ed08c2c2832179",
+    "url": "/coupon-advertising/static/js/2.1d89d5df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coupon-advertising/static/js/2.1d89d5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3e0730e4e86487e1af",
+    "url": "/coupon-advertising/static/js/main.29bc4ed7.chunk.js"
+  },
+  {
+    "revision": "74e987cfe9aa4179da00",
+    "url": "/coupon-advertising/static/js/runtime-main.a9c07907.js"
+  }
+]);
